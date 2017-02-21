5198c73dd4c1cd6ab84306f7d00dc0fb2fc7c3c2
Ken Lee <wewert@gmail.com>
2017-02-21 13:45:13 -0700

    Test

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,8 +1,33 @@
+require 'pry'
 class Scrabble
 
   def score(word)
-    1
+    letters = word.to_s.upcase.split('')
+    letters.inject(0) do |accumulator, letter|
+      accumulator += score_letter(letter)
+    end
   end
+
+  def score_letter(letter)
+    point_values[letter]
+  end
+  #
+  # def highest_score_from(list)
+  #   score, top_words = grouped_by_score(list).max
+  #   bonus_word_from(top_words) || shortest_from(top_words)
+  # end
+  #
+  # def bonus_word_from(words)
+  #   words.detect{|word| word.length == 7}
+  # end
+  #
+  # def shortest_from(words)
+  #   words.min_by{|word| word.length}
+  # end
+  #
+  # def grouped_by_score(list)
+  #   list.group_by{|word| score(word)}
+  # end
 
   def point_values
     {
@@ -15,4 +40,5 @@ class Scrabble
       "Y"=>4, "Z"=>10
     }
   end
-end
+
+  end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -1,12 +1,12 @@
 gem 'minitest'
 require_relative '../lib/scrabble'
 require 'minitest/autorun'
-require 'minitest/emoji'
+require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
+    #skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
